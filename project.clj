0c61520ca01e86288d39d102fe0bed40d717ea5b
Tim McCormack <cortex@brainonfire.net>
2016-06-28 19:24:01 -0400

    dep upgrades

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [aleph "0.4.0"]
-                 [cheshire "5.5.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;; v1
+                 [aleph "0.4.1"]
+                 [cheshire "5.6.3"]
+                 ;; v2
+                 ]
   :main ^:no-aot org.timmc.heim-logger.v1
   :jvm-opts ["-Xmx100M"]
   )
