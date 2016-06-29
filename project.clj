3bbb6772eebb8652b7e0536df99a119393ecd9fe
Tim McCormack <cortex@brainonfire.net>
2016-06-28 22:56:18 -0400

    The start of a v2 client, using a callback-based websocket client lib.

--- project.clj
+++ project.clj
@@ -4,10 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;; v1
-                 [aleph "0.4.1"]
                  [cheshire "5.6.3"]
+
+                 ;; v1
+
+                 [aleph "0.4.1"]
+
                  ;; v2
+
+                 ;; I had trouble with http.async.client -- I got a
+                 ;; 403 when connecting to euphoria.io.
+                 [stylefruits/gniazdo "1.0.0"]
                  ]
   :main ^:no-aot org.timmc.heim-logger.v1
   :jvm-opts ["-Xmx100M"]
---
+++ src/org/timmc/heim_logger/v2.clj
@@ -0,0 +1,92 @@
+(ns org.timmc.heim-logger.v2
+  "Version 2 of logger."
+  (:require [cheshire.core :as json]
+            [gniazdo.core :as ws]
+            [org.timmc.heim-logger.common :as cm]))
+
+(defn send-msg
+  [state msg]
+  (ws/send-msg @(:socket @state)
+               (json/generate-string msg)))
+
+(defn on-connect
+  [state session]
+  (send-msg state {:type "nick"
+                   :data {:name "hillbot v2"}}))
+
+(defn on-receive
+  [state raw]
+  (let [msg (try
+              (json/parse-string raw true)
+              (catch Throwable t
+                (println "Raw message was:" raw)
+                (throw t)))
+        mtype (:type msg)
+        data (:data msg)]
+    (case mtype
+      "ping-event"
+      (send-msg state
+                {:type "ping-reply",
+                 :data {:time (:time data)}})
+
+      "send-event"
+      (let [{:keys [sender, content]} data
+            {nick :name, id :id} sender]
+        (println (format "RECV %s(%s) -> %s"
+                         nick id content)))
+
+      "join-event"
+      (println "JOIN" (:name data))
+      
+      "part-event"
+      (println "PART" (:name data))
+      
+      "nick-event"
+      (println "NICK" (:from data) "->" (:to data))
+      
+      ;; :else
+      (println "received" mtype))))
+
+(defn test-gniazdo
+  []
+  (let [state (atom {:session nil
+                     :socket (promise)})
+        socket (ws/connect
+                (cm/address "euphoria.io" "space")
+                :on-connect (partial on-connect state)
+                :on-receive (partial on-receive state))]
+    ;; This is how we tie the asynchronous knot: Block any use of the
+    ;; socket until we have time to set it! (Otherwise there's a race
+    ;; condition where we might want to send a reply before reaching
+    ;; this line -- would require a fast connection and a long context
+    ;; switch or GC pause, of course.)
+    (deliver (:socket @state) socket)
+    state))
+
+;; --
+
+(defn start
+  "Start logger and return a session object."
+  [server room logfile]
+  (println "starting" room)
+  (.start (Thread. (while true
+                     5)))
+  {:room room})
+
+(defn stop
+  "Halt logger using session object."
+  [session]
+  (println "halting" session)
+  (flush))
+
+(defn -main
+  "Run logger and close it on shutdown. Arguments:
+
+- server is the heim host (requires secure websocket protocol)
+- room is the heim room (without ampersand)
+- logfile is the file where logs will be written to (and read from for
+  catchup)"
+  [server room logfile]
+  (let [session (start server room logfile)]
+    (.addShutdownHook (Runtime/getRuntime)
+                      (Thread. #(stop session) "logger shutdown hook"))))
